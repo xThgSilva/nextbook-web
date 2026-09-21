e9bb52131599e6e0bd5ae9a7dba8382663362c5f
xThgSilva <camposthiago2021@gmail.com>
2026-09-20 23:54:06 -0300

    feat: register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom"
+
 import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
 
   return (
     <>
-    <Login />
+      <Routes>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
     </>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,11 @@
-import "../styles/login.css"
+import "../styles/auth.css"
 import { Link } from "react-router-dom"
 
 import { FaBookReader } from "react-icons/fa"
 
 const Login = () => {
     return (
-        <div className="login-page-container">
+        <div className="auth-page-container">
             <section className="logo-section">
                 <div className="logo-content">
                     <FaBookReader className="logo-icon" />
@@ -13,17 +13,17 @@ const Login = () => {
                 </div>
                 <p className="tagline">Borrow. Buy. Explore.</p>
             </section>
-            <div className="login-container">
+            <div className="auth-container">
                 <div className="form-header">
                     <h2>Welcome Back</h2>
                     <p>Good to see you again!</p>
                 </div>
-                <form className="login-form">
-                    <div>
+                <form className="auth-form">
+                    <div className="input-group">
                         <label htmlFor="email-input">Your e-mail</label>
                         <input type="email" id="email-input" placeholder="myemail@email.com" />
                     </div>
-                    <div>
+                    <div className="input-group">
                         <label htmlFor="password-input">Your password</label>
                         <input type="password" id="password-input" placeholder="myemail@email.com" />
                     </div>
