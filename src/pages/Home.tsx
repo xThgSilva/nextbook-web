91e6880053a946a3924ab78851e1ee129d6aae91
xThgSilva <camposthiago2021@gmail.com>
2026-09-22 23:26:45 -0300

    feat: hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
+import "../styles/home.css";
+
 import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <>
+    <div className="home-page-container">
       <Header />
-    </>
+      <section className="hero-section">
+        <div className="hero-text">
+          <h1>NextBook</h1>
+          <p>NextBook is your online library and stationery store. Find books from different categories, or rent them with flexible deadlines. Just don't forget to return them!</p>
+        </div>
+        <div className="" style={{background: "lightgray"}}>
+          Future image
+        </div>
+      </section>
+    </div>
   )
 }
 
